2b715a747c3886c52d4586f1d542bf2b0d417c4b
Ayush Shrivastava <shrivastava.ayush1996@gmail.com>
2017-12-18 12:33:18 +0530

    Changed parameters

--- evaluate_models.sh
+++ evaluate_models.sh
@@ -5,11 +5,11 @@
 DATASET_DIR=~/SS/Datasets/cityscapes-images/
 DATA_LIST=list/eval_list.txt
 CHECKPOINT_FOLDER=logs/train1-Fine-Full-Momentum/
-EPOCHS=43
+EPOCHS='43'
 GPU=1
 
-CHECKPOINT_FILE = $CHECKPOINT_FOLDER'model.ckpt-'$EPOCH
-EVALUATE_LOG_FILE = CHECKPOINT_FOLDER'evaluate.log'
+CHECKPOINT_FILE=$CHECKPOINT_FOLDER'model.ckpt-'$EPOCHS
+EVALUATE_LOG_FILE=$CHECKPOINT_FOLDER'evaluate.log'
 
 
 python evaluate.py \
