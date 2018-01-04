c9cc7a17c15cd0a2e5d054df275d5e241f648a2a
Ayush Shrivastava <shrivastava.ayush1996@gmail.com>
2018-01-04 11:53:55 +0530

    Cleaning code

--- prepare_initialisation_weights.py
+++ prepare_initialisation_weights.py
@@ -15,7 +15,7 @@ tf.app.flags.DEFINE_string(
 
 tf.app.flags.DEFINE_string(
     'save_model', 'MobileNetPSP',
-    'Directory where weights are to be saved')
+    'Directory where initialised weights are to be saved')
 
 tf.app.flags.DEFINE_boolean('print_architecture', True,
                             'Print architecure.')
@@ -27,23 +27,6 @@ FLAGS = tf.app.flags.FLAGS
 
 #Set Visible CUDA Devices
 os.environ['CUDA_VISIBLE_DEVICES'] = '{}'.format(FLAGS.gpu)
-
-def weights_initialisers():
-    new_variables = ['MobileNet/conv_ds_15a',
-                     'MobileNet/conv_ds_15b',
-                     'MobileNet/conv_ds_15c',
-                     'MobileNet/conv_ds_15d',
-                     'MobileNet/conv_ds_16',
-                     'MobileNet/conv_ds_17']
-    restoreVar_mobilenet = slim.get_variables_to_restore(include=['MobileNet'],exclude=new_variables)
-    # restoreVar_mobilenet = [v for v in restoreVar_mobilenet if 'Momentum' not in v.name]
-    newLayerVariables = slim.get_variables_to_restore(include=new_variables)
-
-    checkpoint_path=FLAGS.pretrained_check_point
-    print('Restoring weights from  %s: ' % (checkpoint_path) )
-    readMobileNetWeights = slim.assign_from_checkpoint_fn(checkpoint_path,restoreVar_mobilenet)
-    otherLayerInitializer = tf.variables_initializer(newLayerVariables)
-    return readMobileNetWeights, otherLayerInitializer
 
 def save(saver, sess, logdir):
     model_name = 'model.ckpt'
@@ -75,19 +58,11 @@ def main():
 
     var_list = tf.global_variables()
 
-    # init = tf.global_variables_initializer()
-
-    # loader = tf.train.Saver(var_list=restoreVar_mobilenet)
-    # loader.restore(sess, FLAGS.pretrained_check_point)
-
-
     # Set up tf session and initialize variables.
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
 
     with tf.Session(config=config) as sess:
-        # init = tf.global_variables_initializer()
-        # sess.run(init)
 
         loader = tf.train.Saver(var_list=restoreVar_mobilenet)
         loader.restore(sess, FLAGS.pretrained_mobilenet)
